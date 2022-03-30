e6379b8add2a1acef700ea812e8e1a228f0fb8f0
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-30 14:21:27 +0200

    added header

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <img class="logo" src="../assets/img/spotify.png" alt="">
+    </header>
+</template>
+
+<script>
+export default {
+    name: "HeaderSpotify"
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/style.scss";
+
+    header{
+        height: 10vh;
+        background-color: $lightGrey;
+        display: flex;
+        align-items: center;
+
+        .logo{
+            height: 60%;
+            padding-left: 1rem;
+        }
+    }
+</style>
